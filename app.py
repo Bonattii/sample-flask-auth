0c37a77043b978c44cffdea5968aec8efa61b296
Rodrigo Bonatti <rodrigo@Rodrigos-MacBook-Pro.local>
2024-02-16 15:20:12 -0800

    Create login route

--- app.py
+++ app.py
@@ -1,16 +1,39 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from models.user import User
 from database import db
+from flask_login import LoginManager, login_user
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 
-db.init_app(app)
+login_manager = LoginManager()
 
-@app.route("/hello-world", methods=["GET"])
-def hello_world():
-  return "Hello, World!"
+db.init_app(app)
+login_manager.init_app(app)
+
+# Login View
+login_manager.login_view = "login"
+
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(user_id)
+
+@app.route("/login", methods=["POST"])
+def login():
+  data = request.json
+  username = data.get("username")
+  password = data.get("password")
+
+  if username and password:
+    # Login
+    user = User.query.filter_by(username=username).first()
+
+    if user and user.password == password:
+      login_user(user)
+      return jsonify({"message": "Successfully authenticated"})
+
+  return jsonify({"message": "Invalid credentials"}), 400
 
 if __name__ == "__main__":
   app.run(debug=True)
